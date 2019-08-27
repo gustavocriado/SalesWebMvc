28ff9d0299fb35c2d74de442446bd35bf06f9c87
Gustavo Lira Criado <gustavoliracriado30@gmail.com>
2019-08-27 14:08:33 -0300

    continuação do commit interior

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SalesWebMvc.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace SalesWebMvc.Models
 {
     public class Product
     {
+        private int id { get; set; }
+        private string Name { get; set; }
+        private int Quantity { get; set; }
+        private string Description { get; set; }
+        public ModelProduct Model { get; set; }
+        public CategoryProduct Category { get; set; }
+        public Sales Sales { get; set; }
     }
 }
